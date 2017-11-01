e7d4c456de0b22d11c5133991440b4c2698fc51c
Panos Tolios <panostolios@gmail.com>
2017-11-01 21:03:19 +0200

    FolderItem contains its contents
    FolderItem populates itself with the FolderItems it contains
    on initialization

--- folders/__init__.py
+++ folders/__init__.py
@@ -3,19 +3,33 @@ import os
 
 class FolderItem:
 
-    def __new__(cls, path):
+    def __new__(cls, path: str):
         if os.path.exists(path):
             return super(FolderItem, cls).__new__(cls)
         else:
             raise ValueError('File or Folder does not exist')
 
-    def __init__(self, path):
-        self.path = os.path.abspath(path)
-        self.is_dir = os.path.isdir(self.path)
-        self.items = os.listdir(self.path)
-        self.absitems = [os.path.join(self.path, item) for item in self.items]
+    def __init__(self, path: str):
+        self.path: str = os.path.abspath(path)
+        self.name: str = os.path.basename(self.path)
+        self.is_dir: bool = os.path.isdir(self.path)
+        self.list_items: list = []
+        self.item_paths: list = []
+        self.items: list = []
+
+        if self.is_dir:
+            self.list_items = os.listdir(self.path)
+            self.item_paths = [
+                os.path.join(self.path, item) for item in self.list_items
+            ]
+            self.items = [
+                FolderItem(item_path) for item_path in self.item_paths
+            ]
 
     @property
     def item_type(self):
         return 'directory' if self.is_dir else 'file'
+
+    def __repr__(self):
+        return f"<FolderItem {self.item_type}:' {self.name}'>"
 
